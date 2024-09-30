8082925396f1918337c02776db539e3ab32cb23d
DKormann <dominikormann@outlook.com>
2024-09-30 14:50:37 +0200

    ccleanup

--- dataloader.py
+++ dataloader.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 rawdata = csv.reader(open('./dataset/test.csv', 'r'))
+title = next(rawdata)
 rawdata = list(rawdata)
 
 #%%
 
 def getslice(head:str):
-  member = [i for i, item in enumerate(rawdata[0]) if item.startswith(head)]
+  member = [i for i, item in enumerate(title) if item.startswith(head)]
   return slice(member[0], member[-1]+1)
 
 singlecolumns = ['id', 'p_num', 'time']
@@ -20,27 +21,28 @@ dataslices = { col: getslice(col) for col in columns}
 for col in dataslices: print(f'{col}: {", ".join(rawdata[1][dataslices[col]][:10])}')
 
 #%%
+activity_labels = list(set(act for dataline in rawdata for act in dataline[dataslices['activity']]))
 
+data = {
+  col : np.array([[float(s) if s else np.nan for s in dataline[dataslices[col]]] for dataline in rawdata[1:]])
+  for col in datacolumns if col !='activity'
+}
+
+data['activities'] = np.array([[activity_labels.index(act) if act in activity_labels else 0 for act in raw[dataslices["activity"]]] for raw in rawdata])
+
+for col in singlecolumns: data[col] = np.array([dataline[dataslices[col]][0] for dataline in rawdata[1:]])
+
+#%%
 class Dataset:
 
   def __init__(self, data):
     self.data = data
-  
-  @staticmethod
-  def from_rawdata(rawdata=rawdata):
-    return Dataset({
-      col : np.array([[float(s) if s else np.nan for s in dataline[dataslices[col]]] for dataline in rawdata[1:]])
-      for col in datacolumns if col !='activity'
-    })
 
   def __getattribute__(self, name: str):
     if name in ['data']: return super().__getattribute__(name)
     return self.data[name]
   
-  __slots__ = ['data', 'bg', 'insulin', 'carbs', 'hr', 'steps', 'cals']
+  __slots__ = ['data', 'bg', 'insulin', 'carbs', 'hr', 'steps', 'cals', 'activity', 'activities']
   
-
-data = Dataset.from_rawdata()
-assert data.bg.shape == data.carbs.shape
-
-# %%
+train_data = Dataset(data)
+assert train_data.bg.shape == train_data.carbs.shape
